cc3ed7156a312a54c4d487d4a6a107aa63dfa1a3
ZareMate <70139322+ZareMate@users.noreply.github.com>
2026-09-25 21:14:36 +0200

    Add sign packet interception

---
+++ src/main/java/com/zaremate/keycheck/mixin/ServerGamePacketListenerImplMixin.java
@@ -0,0 +1,23 @@
+package com.zaremate.keycheck.mixin;
+
+import com.zaremate.keycheck.KeyCheckEvents;
+import net.minecraft.network.protocol.game.ServerboundSignUpdatePacket;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerGamePacketListenerImpl.class)
+public abstract class ServerGamePacketListenerImplMixin {
+    @Inject(method = "handleSignUpdate", at = @At("HEAD"), cancellable = true)
+    private void keycheck$handleSignUpdate(ServerboundSignUpdatePacket packet, CallbackInfo ci) {
+        ServerGamePacketListenerImpl listener = (ServerGamePacketListenerImpl) (Object) this;
+        if (!KeyCheckEvents.isExpectedPacket(listener.player, packet))
+            return;
+
+        listener.player.server.execute(() ->
+                KeyCheckEvents.handleSignResponse(listener.player, packet));
+        ci.cancel();
+    }
+}
