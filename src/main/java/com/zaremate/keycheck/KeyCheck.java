b9f1398e0f487afe7840e6bdb0fc416a7486a985
ZareMate <70139322+ZareMate@users.noreply.github.com>
2026-09-25 21:14:31 +0200

    Add main mod class

---
+++ src/main/java/com/zaremate/keycheck/KeyCheck.java
@@ -0,0 +1,20 @@
+package com.zaremate.keycheck;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.NeoForge;
+import org.slf4j.Logger;
+
+@Mod(KeyCheck.MOD_ID)
+public final class KeyCheck {
+    public static final String MOD_ID = "keycheck";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public KeyCheck(ModContainer container) {
+        container.registerConfig(ModConfig.Type.COMMON, KeyCheckConfig.SPEC);
+        NeoForge.EVENT_BUS.register(KeyCheckEvents.class);
+        LOGGER.info("KeyCheck loaded. Blacklisted keys: {}", KeyCheckConfig.blacklistedKeys());
+    }
+}
