29118042c797df7e1d1230bd2ec45f4c8391e95e
ZareMate <70139322+ZareMate@users.noreply.github.com>
2026-09-26 03:11:18 +0200

    Rename Airport Security System network classes

--- src/main/java/com/zaremate/keycheck/network/KeyCheckConfigStartPayload.java
+++ src/main/java/com/zaremate/keycheck/network/KeyCheckConfigStartPayload.java
@@ -1,19 +1,19 @@
 package com.zaremate.airport_security_system.network;
 
-import com.zaremate.airport_security_system.KeyCheck;
+import com.zaremate.airport_security_system.AirportSecuritySystem;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 
-public final class KeyCheckConfigStartPayload implements CustomPacketPayload {
-    public static final Type<KeyCheckConfigStartPayload> TYPE =
-            new Type<>(ResourceLocation.fromNamespaceAndPath(KeyCheck.MOD_ID, "config_start"));
-    public static final KeyCheckConfigStartPayload INSTANCE = new KeyCheckConfigStartPayload();
-    public static final StreamCodec<FriendlyByteBuf, KeyCheckConfigStartPayload> STREAM_CODEC =
+public final class AirportSecuritySystemConfigStartPayload implements CustomPacketPayload {
+    public static final Type<AirportSecuritySystemConfigStartPayload> TYPE =
+            new Type<>(ResourceLocation.fromNamespaceAndPath(AirportSecuritySystem.MOD_ID, "config_start"));
+    public static final AirportSecuritySystemConfigStartPayload INSTANCE = new AirportSecuritySystemConfigStartPayload();
+    public static final StreamCodec<FriendlyByteBuf, AirportSecuritySystemConfigStartPayload> STREAM_CODEC =
             StreamCodec.unit(INSTANCE);
 
-    private KeyCheckConfigStartPayload() {}
+    private AirportSecuritySystemConfigStartPayload() {}
 
     @Override
     public Type<? extends CustomPacketPayload> type() {
