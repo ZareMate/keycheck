495235d71e375285ac7c9b1db4a82a798c12bca9
ZareMate <70139322+ZareMate@users.noreply.github.com>
2026-09-26 03:07:44 +0200

    Rename client and network identifiers

--- src/main/java/com/zaremate/keycheck/network/KeyCheckStatusPayload.java
+++ src/main/java/com/zaremate/keycheck/network/KeyCheckStatusPayload.java
@@ -1,6 +1,6 @@
-package com.zaremate.airport_security.network;
+package com.zaremate.airport_security_system.network;
 
-import com.zaremate.airport_security.KeyCheck;
+import com.zaremate.airport_security_system.KeyCheck;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
