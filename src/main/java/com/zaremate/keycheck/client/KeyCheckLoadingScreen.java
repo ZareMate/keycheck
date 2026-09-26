a0df8303275ec9942bbc74d7d428df56d80e68fa
ZareMate <70139322+ZareMate@users.noreply.github.com>
2026-09-26 03:07:35 +0200

    Rename client and network identifiers

--- src/main/java/com/zaremate/keycheck/client/KeyCheckLoadingScreen.java
+++ src/main/java/com/zaremate/keycheck/client/KeyCheckLoadingScreen.java
@@ -1,4 +1,4 @@
-package com.zaremate.airport_security.client;
+package com.zaremate.airport_security_system.client;
 
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
@@ -6,7 +6,7 @@ import net.minecraft.network.chat.Component;
 
 public final class KeyCheckLoadingScreen extends Screen {
     public KeyCheckLoadingScreen() {
-        super(Component.literal("Airport Security"));
+        super(Component.literal("Airport Security System"));
     }
 
     @Override
