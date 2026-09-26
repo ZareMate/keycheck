7b2cb2277f7bf56d62339513a01b9c47e774c63d
ZareMate <70139322+ZareMate@users.noreply.github.com>
2026-09-26 03:05:16 +0200

    Finish airport_security rename

--- src/main/java/com/zaremate/keycheck/mixin/SignBlockEntityAccessor.java
+++ src/main/java/com/zaremate/keycheck/mixin/SignBlockEntityAccessor.java
@@ -1,4 +1,4 @@
-package com.zaremate.keycheck.mixin;
+package com.zaremate.airport_security.mixin;
 
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.entity.SignText;
@@ -10,11 +10,11 @@ import java.util.UUID;
 @Mixin(SignBlockEntity.class)
 public interface SignBlockEntityAccessor {
     @Accessor("frontText")
-    void keycheck$setFrontText(SignText text);
+    void airport_security$setFrontText(SignText text);
 
     @Accessor("backText")
-    void keycheck$setBackText(SignText text);
+    void airport_security$setBackText(SignText text);
 
     @Accessor("playerWhoMayEdit")
-    void keycheck$setEditor(UUID editor);
+    void airport_security$setEditor(UUID editor);
 }
