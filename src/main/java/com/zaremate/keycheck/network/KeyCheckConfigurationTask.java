4ea3f3e80b352861cbbafff303aa1f54b174f7e7
ZareMate <70139322+ZareMate@users.noreply.github.com>
2026-09-26 03:11:20 +0200

    Rename Airport Security System network classes

--- src/main/java/com/zaremate/keycheck/network/KeyCheckConfigurationTask.java
+++ src/main/java/com/zaremate/keycheck/network/KeyCheckConfigurationTask.java
@@ -1,15 +1,15 @@
 package com.zaremate.airport_security_system.network;
 
-import com.zaremate.airport_security_system.KeyCheck;
+import com.zaremate.airport_security_system.AirportSecuritySystem;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.network.configuration.ICustomConfigurationTask;
 
 import java.util.function.Consumer;
 
-public record KeyCheckConfigurationTask() implements ICustomConfigurationTask {
+public record AirportSecuritySystemConfigurationTask() implements ICustomConfigurationTask {
     public static final Type TYPE =
-            new Type(ResourceLocation.fromNamespaceAndPath(KeyCheck.MOD_ID, "client_check"));
+            new Type(ResourceLocation.fromNamespaceAndPath(AirportSecuritySystem.MOD_ID, "client_check"));
 
     @Override
     public Type type() {
@@ -18,6 +18,6 @@ public record KeyCheckConfigurationTask() implements ICustomConfigurationTask {
 
     @Override
     public void run(Consumer<CustomPacketPayload> sender) {
-        sender.accept(KeyCheckConfigStartPayload.INSTANCE);
+        sender.accept(AirportSecuritySystemConfigStartPayload.INSTANCE);
     }
 }
