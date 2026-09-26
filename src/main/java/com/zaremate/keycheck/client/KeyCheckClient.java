75645c8877095cf9f1e243ac12874392f6ed31f7
ZareMate <70139322+ZareMate@users.noreply.github.com>
2026-09-26 03:07:30 +0200

    Rename client and network identifiers

--- src/main/java/com/zaremate/keycheck/client/KeyCheckClient.java
+++ src/main/java/com/zaremate/keycheck/client/KeyCheckClient.java
@@ -164,7 +164,7 @@ public final class KeyCheckClient {
 
         graphics.drawCenteredString(
                 minecraft.font,
-                "Airport Security System",
+                "Airport Security System System",
                 width / 2,
                 top + 14,
                 0xFFFFFFFF
