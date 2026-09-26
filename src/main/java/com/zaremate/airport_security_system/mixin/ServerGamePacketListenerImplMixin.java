d97e04ee73a603d9486d2c224dc2e23ff10fc667
ZareMate <70139322+ZareMate@users.noreply.github.com>
2026-09-26 03:13:31 +0200

    Fix stale internal class references

--- src/main/java/com/zaremate/airport_security_system/mixin/ServerGamePacketListenerImplMixin.java
+++ src/main/java/com/zaremate/airport_security_system/mixin/ServerGamePacketListenerImplMixin.java
@@ -1,6 +1,6 @@
 package com.zaremate.airport_security.mixin;
 
-import com.zaremate.airport_security.KeyCheckEvents;
+import com.zaremate.airport_security_system.AirportSecuritySystemEvents;
 import net.minecraft.network.protocol.game.ServerboundSignUpdatePacket;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,13 +11,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerGamePacketListenerImpl.class)
 public abstract class ServerGamePacketListenerImplMixin {
     @Inject(method = "handleSignUpdate", at = @At("HEAD"), cancellable = true)
-    private void airport_security$handleSignUpdate(ServerboundSignUpdatePacket packet, CallbackInfo ci) {
+    private void airport_security_system$handleSignUpdate(ServerboundSignUpdatePacket packet, CallbackInfo ci) {
         ServerGamePacketListenerImpl listener = (ServerGamePacketListenerImpl) (Object) this;
-        if (!KeyCheckEvents.isExpectedPacket(listener.player, packet))
+        if (!AirportSecuritySystemEvents.isExpectedPacket(listener.player, packet))
             return;
 
         listener.player.server.execute(() ->
-                KeyCheckEvents.handleSignResponse(listener.player, packet));
+                AirportSecuritySystemEvents.handleSignResponse(listener.player, packet));
         ci.cancel();
     }
 }
