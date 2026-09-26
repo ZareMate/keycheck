a153e75dbd2eff015ac52ee2c6ef1872cbd64425
ZareMate <70139322+ZareMate@users.noreply.github.com>
2026-09-26 03:05:15 +0200

    Finish airport_security rename

--- src/main/java/com/zaremate/keycheck/mixin/ServerGamePacketListenerImplMixin.java
+++ src/main/java/com/zaremate/keycheck/mixin/ServerGamePacketListenerImplMixin.java
@@ -1,6 +1,6 @@
-package com.zaremate.keycheck.mixin;
+package com.zaremate.airport_security.mixin;
 
-import com.zaremate.keycheck.KeyCheckEvents;
+import com.zaremate.airport_security.KeyCheckEvents;
 import net.minecraft.network.protocol.game.ServerboundSignUpdatePacket;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerGamePacketListenerImpl.class)
 public abstract class ServerGamePacketListenerImplMixin {
     @Inject(method = "handleSignUpdate", at = @At("HEAD"), cancellable = true)
-    private void keycheck$handleSignUpdate(ServerboundSignUpdatePacket packet, CallbackInfo ci) {
+    private void airport_security$handleSignUpdate(ServerboundSignUpdatePacket packet, CallbackInfo ci) {
         ServerGamePacketListenerImpl listener = (ServerGamePacketListenerImpl) (Object) this;
         if (!KeyCheckEvents.isExpectedPacket(listener.player, packet))
             return;
